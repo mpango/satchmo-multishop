ed90be1516fa55ee543e567c0563c204fe657416
Dennis Schmidt <dmrschmidt@gmail.com>
2012-01-21 15:28:10 +0100

    selection

--- store/multishop/views.py
+++ store/multishop/views.py
@@ -16,15 +16,22 @@ import urllib
 
 log = logging.getLogger('satchmo_store.shop.views')
 
+from django import forms
+class ProductSelectionForm(forms.Form):
+	products = forms.ModelMultipleChoiceField(queryset=Product.objects.none(),
+		widget=forms.CheckboxSelectMultiple)
+
+
 
 #
 # TODO: perform a proper auth check here! (maybe using django-rulez?)
 @login_required
 def selection_index(request):
 	"""docstring for multishop_selection_index"""
-	productlist_url = reverse('multishop.views.productlist')
+	# productlist_url = reverse('multishop.views.productlist')
 	return render_to_response('multishop/selection_index.html', {
-		'productlist_url': productlist_url
+		# 'productlist_url': productlist_url,
+		'products': Product.objects.all()
 	}, context_instance=RequestContext(request))
 
 
@@ -61,29 +68,20 @@ def categorylist(request):
 #
 # TODO: perform a proper auth check here! (maybe using django-rulez?)
 @login_required
-def productlist(request):
-	r = ['<ul class="productList">']
-	try:
-		r = ['<ul class="productList">']
-		shop_id = urllib.unquote(request.POST.get('dir', ''))
-		if shop_id == '':
-			for site in Site.objects.all():
-				r.append('<li class="directory collapsed"><a href="#" rel="%s/">%s</a></li>' %(site.id, site.name))
-		else:
-			try:
-				shop_id = shop_id[:-1]
-				site = Site.objects.get(id__exact=int(shop_id))
-				for site in Category.objects.by_site(site):
-					r.append('<li class="file"><a href="#" rel="%s/">%s</a></li>' %(site.id, site.name))
-			except Exception, e:
-				r.append('Could not load shop: %s' % str(e))
-		# r.append('<li class="directory collapsed"><a href="#" rel="%s/">%s</a></li>' % (ff,f))
-		# r.append('<li class="file ext_%s"><a href="#" rel="%s">%s</a></li>' % (e,ff,f))
-		r.append('</ul>')
-	except Exception, e:
-		r.append('Could not load index: %s' % str(e))
-	r.append('</ul>')
-	return HttpResponse(''.join(r))
+def productlist(request, cat_id):
+	if request.method == 'POST': # If the form has been submitted...
+		form = ProductSelectionForm(request.POST) # A form bound to the POST data
+		form.products.queryset = Product.objects.filter(category__id__exact=cat_id)
+		if form.is_valid(): # All validation rules pass
+			# Process the data in form.cleaned_data
+			# ...
+			return HttpResponseRedirect('/thanks/') # Redirect after POST
+	else:
+		form = ProductSelectionForm() # An unbound form
+		form.fields['products'].queryset = Product.objects.filter(category__id__exact=cat_id)
 
+	return render_to_response('multishop/_productlist.html', {
+		'form': form,
+	}, context_instance=RequestContext(request))
 
 
